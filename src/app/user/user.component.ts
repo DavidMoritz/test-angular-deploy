151916fd88d10027e31e6434177b6c162e30cd2a
David Moritz <david@jde-systems.com>
2024-06-17 22:33:00 -0500

    output events

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,10 @@
-import { Component, computed, input } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Output,
+  computed,
+  input,
+} from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -11,12 +17,16 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
+  id = input.required<string>();
   avatar = input.required<string>();
   name = input.required<string>();
+  @Output() select = new EventEmitter();
 
   avatarPath = computed(() => {
     return 'assets/users/' + this.avatar();
   });
 
-  onSelectUser() {}
+  onSelectUser() {
+    this.select.emit(this.id());
+  }
 }
